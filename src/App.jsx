6be6ff91997ed2a59eeb9a08889359b48422aca2
iftekar2 <iftekar.hossain123@gmail.com>
2023-07-09 11:08:25 -0400

    Creating Components Folder and adding all the Files that I might need

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Feed from "../components/Feed";
+import ChannelDetail from "../components/ChannelDetail";
+import Search from "../components/Search";
+import VideoDetail from "../components/VideoDetail";
 
 function App() {
-  return <div>Hello React</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Feed />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
