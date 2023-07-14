e482e3a035607d2a42d4d435ea5e1f0fc5452bc2
iftekar2 <iftekar.hossain123@gmail.com>
2023-07-14 19:57:31 -0400

    Mading Sidebar Section

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import "./Style.css";
+import Sidebar from "./Sidebar";
 
 function Feed() {
-  return <div>Feed</div>;
+  return (
+    //This is Feed Page
+    <section className="feedPage">
+      {/* This is sideNavbar Section */}
+      <section className="sideNavbar">
+        <div className="sideBar">
+          <Sidebar />
+          <p className="copyRight">Copyright 2023 Iftekar Hossain</p>
+        </div>
+      </section>
+
+      {/* This is Content Section*/}
+      <section className="content"></section>
+    </section>
+  );
 }
 
 export default Feed;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Style.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
