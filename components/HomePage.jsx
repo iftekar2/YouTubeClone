11a87b5e8170d61022eccad241b8640acde979d3
iftekar2 <iftekar.hossain123@gmail.com>
2023-07-17 23:11:03 -0400

    Finishing Home Page

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import "./Style.css";
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <section className="HomePageSection">
+      <div className="homePage">
+        <div className="homePageIcons">
+          <div className="firstImage">
+            <img src="../Images/barbell.png"></img>
+          </div>
+          <div className="secondImage">
+            <img src="../Images/dumbbell.png"></img>
+          </div>
+          <div className="thirdImage">
+            <img src="../Images/healthy.png"></img>
+          </div>
+          <div className="forthImage">
+            <img src="../Images/treadmill.png"></img>
+          </div>
+          <div className="fifthImage">
+            <img src="../Images/weightlifting.png"></img>
+          </div>
+        </div>
+        <div className="HomeTitle">
+          <h1>FITZONE</h1>
+        </div>
+        <div className="slogan">
+          <p>Improve</p>
+          <p className="dot">·</p>
+          <p>Elevate</p>
+          <p className="dot">·</p>
+          <p>Excelerate</p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default HomePage;
