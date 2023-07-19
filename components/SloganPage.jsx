b9dc606832f8693c776cafac77216eab1fb3538f
iftekar2 <iftekar.hossain123@gmail.com>
2023-07-19 06:36:58 -0400

    Finishing Slogan Page

---
+++ components/SloganPage.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Style.css";
+
+function SloganPage() {
+  return (
+    <section className="sloganSection">
+      <div className="sloganPage">
+        <div className="sloganTitle">
+          <h1>Unleash Your Inner Athlete!</h1>
+        </div>
+        <div className="sloganDescription">
+          <p>
+            Welcome to FitZone, where we help you take control of your fitness
+            journey
+          </p>
+          <p>with our custom workout plans and amazing trainers.</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default SloganPage;
--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import ActionPage from "../components/ActionPage";
 import ExamplePage from "../components/ExamplePage";
 import FAQPage from "../components/FAQPage";
 import FooterPage from "../components/FooterPage";
+import SloganPage from "../components/SloganPage";
 
 function App() {
   return (
     <div className="mainPage">
       <HomePage />
+      <SloganPage />
       <AboutPage />
       <ActionPage />
       <ExamplePage />
