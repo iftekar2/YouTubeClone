380161f2d42835559a9078c6d055e049cc2ef7e8
iftekar2 <iftekar.hossain123@gmail.com>
2023-07-20 05:44:31 -0400

    Adding Style and Comments to Example Page

--- components/ExamplePage.jsx
+++ components/ExamplePage.jsx
@@ -5,56 +5,66 @@ function ExamplePage() {
   return (
     <section className="ExampleSection">
       <div className="examplePage">
-        <div className="exampleDiv">
-          <div className="exampleOne">
-            <img src="../Images/biceps.png" />
-            <div className="workoutName">
-              <h1>Biceps</h1>
-            </div>
-          </div>
+        <div className="exampleTitle">
+          <h2 className="title">Ready to begin your fitness journey?</h2>
         </div>
 
-        <div className="exampleDiv">
-          <div className="exampleTwo">
-            <img src="../Images/chest.png" />
-            <div className="workoutName">
-              <h1>Chest</h1>
-            </div>
-          </div>
+        <div className="exampleDescription">
+          <p>Please choose from the Options bellow!!</p>
         </div>
 
-        <div className="exampleDiv">
-          <div className="exampleThree">
-            <img src="../Images/abdominals.png" />
-            <div className="workoutName">
-              <h1>Abdominals</h1>
+        <div className="exerciseExamples">
+          <div className="exampleDiv">
+            <div className="exampleOne">
+              <img src="../Images/biceps.png" />
+              <div className="workoutName">
+                <h1>Biceps</h1>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="exampleDiv">
-          <div className="exampleFour">
-            <img src="../Images/quads.png" />
-            <div className="workoutName">
-              <h1>Quads</h1>
+          <div className="exampleDiv">
+            <div className="exampleTwo">
+              <img src="../Images/chest.png" />
+              <div className="workoutName">
+                <h1>Chest</h1>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="exampleDiv">
-          <div className="exampleFive">
-            <img src="../Images/lats.png" />
-            <div className="workoutName">
-              <h1>Lats</h1>
+          <div className="exampleDiv">
+            <div className="exampleThree">
+              <img src="../Images/abdominals.png" />
+              <div className="workoutName">
+                <h1>Abdominals</h1>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="exampleDiv">
-          <div className="exampleSix">
-            <img src="../Images/hamstrings.png" />
-            <div className="workoutName">
-              <h1>Hamstrings</h1>
+          <div className="exampleDiv">
+            <div className="exampleFour">
+              <img src="../Images/quads.png" />
+              <div className="workoutName">
+                <h1>Quads</h1>
+              </div>
+            </div>
+          </div>
+
+          <div className="exampleDiv">
+            <div className="exampleFive">
+              <img src="../Images/lats.png" />
+              <div className="workoutName">
+                <h1>Lats</h1>
+              </div>
+            </div>
+          </div>
+
+          <div className="exampleDiv">
+            <div className="exampleSix">
+              <img src="../Images/hamstrings.png" />
+              <div className="workoutName">
+                <h1>Hamstrings</h1>
+              </div>
             </div>
           </div>
         </div>
