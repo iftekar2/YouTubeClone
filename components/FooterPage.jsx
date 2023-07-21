101a28ee2d83b15c2b8e448828980319c6db8adc
iftekar2 <iftekar.hossain123@gmail.com>
2023-07-20 23:14:30 -0400

    Finishing Footer Page

--- components/FooterPage.jsx
+++ components/FooterPage.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import "./Style.css";
 
 function FooterPage() {
-  return <div>FooterPage</div>;
+  return (
+    <section className="FooterSection">
+      <div className="footerPage">
+        <div className="copyRights">
+          <p>© 2023 FitZone</p>
+        </div>
+        <div className="footerIcons">
+          <a
+            className="linkedIn"
+            href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile"
+            target="_blank"
+          >
+            <img src="../Images/linkedin.png" />
+          </a>
+          <a
+            className="portfolio"
+            href="https://inspiring-tartufo-2934f8.netlify.app/"
+            target="_blank"
+          >
+            <img src="../Images/internet.png" />
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default FooterPage;
